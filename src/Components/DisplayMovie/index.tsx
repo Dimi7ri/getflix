e3115dc44d02230ccd9605b6401204dce7d5e036
Dimitri Vasiliev <d.vasiliev@nib.com.au>
2021-10-28 09:11:39 +1100

    Show movie details on title click

--- src/Components/DisplayMovie/index.tsx
+++ src/Components/DisplayMovie/index.tsx
@@ -1,18 +1,36 @@
+import { useState } from 'react';
+
 export interface Movie {
   Title?: string;
   Poster?: string;
   Year?: string;
+  Plot?: string;
+  Genre?: string;
+  Actors?: string;
 }
 
 export const DisplayMovie = (movie: Movie) => {
   const { Title, Poster, Year } = movie;
+  const [showDetails, setShowDetails] = useState<boolean>(false);
 
   if (!movie.Title) return (<></>);
   return (
     <div className='movie'>
-      <div className='title'>{Title} </div>
+      <div className='title' onClick={() => setShowDetails(!showDetails)}>{Title} </div>
       <div className='year'>{Year}</div>
       <img src={Poster} alt={Title} />
+      {showDetails ? <DisplayDetails {...movie} /> : ''}
     </div>
   );
-};
+}
+
+const DisplayDetails = (movie: Movie) => {
+  const { Plot, Genre, Actors } = movie;
+  return (
+    <>
+      <div className="genre">{Genre}</div>
+      <div className="plot">{Plot}</div>
+      <div className="actors">{Actors}</div>
+    </>
+  );
+}
